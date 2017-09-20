9efd4cdfd548e0ef5b47ecd2a0e985e7f15a00ab
John Mwenda <jmwenda56@gmail.com>
2017-09-20 17:26:21 +0300

    initial commit

---
+++ tasks/options/shell.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  registerPackage: {
+    options: {
+      stdout: true
+    },
+    command: 'bower register angular-loading-darthwade-forked https://github.com/johnmwenda/angular-loading-darthwade-fork.git'
+  }
+};
